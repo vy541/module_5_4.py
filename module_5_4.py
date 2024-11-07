5b77932574b2f7ca87783145af8f29b4da61212e
vy541 <uumowd1@icloud.com>
2024-11-07 18:54:55 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,37 @@
+class House:
+
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        instance = object.__new__(cls)
+        args = args[0]
+        cls.houses_history.append(args)
+        return instance
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __del__(self):
+        return print(f'{self.name} снесён, но он останется в истории')
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return (f'Название: {self.name}, '
+                f'Кол-во этажей: {self.number_of_floors}')
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
